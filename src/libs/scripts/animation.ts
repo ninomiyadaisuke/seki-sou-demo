b0c5372127ce454bb5737786fca3b58768a536f5
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-10-25 09:34:11 +0900

    add animation howtosection

--- src/libs/scripts/animation.ts
+++ src/libs/scripts/animation.ts
@@ -26,7 +26,7 @@ export function animateItems(targetClass: string) {
 	const options = {
 		root: null,
 		rootMargin: '0px',
-		threshold: 0.1
+		threshold: 0.2
 	};
 
 	const observer = new IntersectionObserver(entries => {
